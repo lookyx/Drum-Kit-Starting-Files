193e556ee071398f39e56698e689b05169900be0
Ken <ken.albaddani@ymail.com>
2023-09-28 19:45:31 -0700

    Button fixes

---
+++ index.js
@@ -0,0 +1,6 @@
+// Selects button, event listener to button, click, point to function handleClick
+document.querySelector('button').addEventListener("click", handleClick);
+
+function handleClick() {
+    alert("I got clicked!");
+}
