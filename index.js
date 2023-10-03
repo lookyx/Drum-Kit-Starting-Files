ee2884e297dfe6ec718738c17ef4a2561c098d21
Ken <ken.albaddani@ymail.com>
2023-10-02 20:23:59 -0700

    Background image and sound fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 // Selects button, event listener to button, click, point to function handleClick
 document.querySelectorAll('.drum').forEach(function(button) {
     button.addEventListener("click", function() {
-        alert("I got clicked!")
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
     });
 });
 
