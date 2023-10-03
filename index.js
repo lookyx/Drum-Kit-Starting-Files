e29fe061ea56d548512ac3024ad849c03c35547a
Ken <ken.albaddani@ymail.com>
2023-10-02 20:30:39 -0700

    Sound fixes

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 // Selects button, event listener to button, click, point to function handleClick
 document.querySelectorAll('.drum').forEach(function(button) {
     button.addEventListener("click", function() {
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        
     });
 });
 
-
+/*var audio = new Audio('./sounds/tom-1.mp3');
+audio.play();*/
     
 
