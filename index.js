17dcc9673561513548b1cb7e58716dc8f3f82053
Ken <ken.albaddani@ymail.com>
2024-03-23 22:07:32 -0700

    phased

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keypress", function(event) {
     makeSound(event.key);
+
+    buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -52,6 +56,16 @@ function makeSound(key) {
             break;
         default: console.log(buttonInnerHTML);
     }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
 
 /*var audio = new Audio('./sounds/tom-1.mp3');
